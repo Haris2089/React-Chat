f5f54f0b0e7f15ab6d5e03132d3890935a1d7224
Haris2089 <103365232+Haris2089@users.noreply.github.com>
2025-01-11 18:20:09 -0500

    Update firebase.jsx
    DELETED API KEY

--- src/lib/firebase.jsx
+++ src/lib/firebase.jsx
@@ -5,14 +5,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDmOLZ2-XHu-2zA7KUI6evcTl74axXxHVc" ,
-  authDomain: "reactchat-9dcd0.firebaseapp.com",
-  projectId: "reactchat-9dcd0",
-  storageBucket: "reactchat-9dcd0.firebasestorage.app",
-  messagingSenderId: "564548667202",
-  appId: "1:564548667202:web:e467c6a1a3cc7dbf1b2adb"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
